bfe2a215b3b1b4d962d6738d368f8d6872a56ffa
Sebastian Hoehna <Sebastian.Hoehna@gmail.com>
2016-09-05 15:29:57 -0700

    Updating bd sampling scripts

--- RB_DiversificationRate_Sampling_Tutorial/scripts/plot.R
+++ RB_DiversificationRate_Sampling_Tutorial/scripts/plot.R
@@ -1,43 +1,50 @@
+# Install RevGadget if you haven't done so already
+#library(devtools)
+#install_github("revbayes/RevGadgets")
+
 library(RevGadgets)
+
+
 tree <- read.nexus("data/primates.tre")
 
 
-#source("scripts/Plot_EBD_RevBayes.R")
 "uniform"
-
-files <- c("output/primates_uniform_speciation_times.log", "output/primates_uniform_speciation_rates.log", "output/primates_uniform_extinction_times.log", "output/primates_uniform_extinction_rates.log")
-
-rev_out <- rev.process.output(files,tree,burnin=0.25,numIntervals=100)
+rev_out <- rev.process.div.rates(speciation_times_file = "output/primates_uniform_speciation_times.log",
+                                 speciation_rates_file = "output/primates_uniform_speciation_rates.log",
+                                 extinction_times_file = "output/primates_uniform_extinction_times.log",
+                                 extinction_rates_file = "output/primates_uniform_extinction_rates.log",
+                                 tree,
+                                 burnin=0.25,numIntervals=100)
 
 pdf("uniform.pdf")
 par(mfrow=c(2,2))
-rev.plot.output(rev_out)
+rev.plot.div.rates(rev_out)
 dev.off()
 
 
-#plot.EBD("output/primates_uniform","uniform")
 "diversified"
-
-files <- c("output/primates_diversified_speciation_times.log", "output/primates_diversified_speciation_rates.log", "output/primates_diversified_extinction_times.log", "output/primates_diversified_extinction_rates.log")
-
-rev_out <- rev.process.output(files,tree,burnin=0.25,numIntervals=100)
+rev_out <- rev.process.div.rates(speciation_times_file = "output/primates_diversified_speciation_times.log",
+                                 speciation_rates_file = "output/primates_diversified_speciation_rates.log",
+                                 extinction_times_file = "output/primates_diversified_extinction_times.log",
+                                 extinction_rates_file = "output/primates_diversified_extinction_rates.log",
+                                 tree,
+                                 burnin=0.25,numIntervals=100)
 
 pdf("diversified.pdf")
 par(mfrow=c(2,2))
-rev.plot.output(rev_out)
+rev.plot.div.rates(rev_out)
 dev.off()
-
-#plot.EBD("output/primates_diversified","diversified")
 
 
 "empirical"
-
-files <- c("output/primates_empirical_speciation_times.log", "output/primates_empirical_speciation_rates.log", "output/primates_empirical_extinction_times.log", "output/primates_empirical_extinction_rates.log")
-
-rev_out <- rev.process.output(files,tree,burnin=0.25,numIntervals=100)
+rev_out <- rev.process.div.rates(speciation_times_file = "output/primates_empirical_speciation_times.log",
+                                 speciation_rates_file = "output/primates_empirical_speciation_rates.log",
+                                 extinction_times_file = "output/primates_empirical_extinction_times.log",
+                                 extinction_rates_file = "output/primates_empirical_extinction_rates.log",
+                                 tree,
+                                 burnin=0.25,numIntervals=100)
 
 pdf("empirical.pdf")
 par(mfrow=c(2,2))
-rev.plot.output(rev_out)
+rev.plot.div.rates(rev_out)
 dev.off()
-#plot.EBD("output/primates_empirical","empirical")
