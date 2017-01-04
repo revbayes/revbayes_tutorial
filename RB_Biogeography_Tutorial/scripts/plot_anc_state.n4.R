40b7df0cbb98b66dce030d90dbe279fe7ede219b
Michael Landis <mlandis@gmail.com>
2017-01-04 14:16:53 -0500

    bg refs

--- RB_Biogeography_Tutorial/scripts/plot_anc_state.n4.R
+++ RB_Biogeography_Tutorial/scripts/plot_anc_state.n4.R
@@ -6,34 +6,43 @@ if (stable) {
 }
 library(ggtree)
 
-#out_str = "output/epoch"
-out_str = "output/simple"
+out_str = "output/epoch"
+#out_str = "output/simple"
 tree_fn = paste(out_str, ".ase.tre", sep="")
 
 st_lbl = c("K","O","M","H",
            "KO","KM","OM","KH","OH","MH",
            "KOM","KOH","KMH","OMH", 
            "KOMH")
+ #        c("R","K","O","M","H","Z",
+ #          "RK",
+ #          "RO","KO",
+ #          "RM","KM","OM",
+ #          "RH","KH","OH","MH",
+ #          "RZ","KZ","OZ","MZ","HZ")
 
-
-st_colors = c(  "#1b9e77",
+st_colors = c(  #"#1b9e77",
                 "#d95f02",
                 "#7570b3",
                 "#e7298a",
                 "#66a61e",
-                "#e6ab02",
-                "#a6761d",
-                "#666666",
+                #"#e6ab02",
+                #"#a6761d",
+                #"#666666",
                 "#66c2a5",
-                "#fc8d62",
+                #"#fc8d62",
                 "#8da0cb",
                 "#e78ac3",
-                "#a6d854",
+                #"#a6d854",
                 "#ffd92f",
                 "#e5c494",
-                "#b3b3b3")
-#,
-#                "#e41a1c",
+                "#b3b3b3",
+                "#e41a1c",
+                "#377eb8",
+                "#4daf4a",
+                "#984ea3",
+                "#ff7f00",
+                "#e41a1c")
  #               "#377eb8",
   #              "#4daf4a",
    #             "#984ea3",
@@ -44,18 +53,19 @@ names(st_colors) = st_lbl
 
 
 summary_statistic = "MAPRange"
-summary_statistic = "PieRange"
+s = 5 / 15
 plot_ancestral_states(tree_file=tree_fn,
                       include_start_states=T,
                       shoulder_label_size=1.5,
-                      shoulder_label_nudge_x=-0.05,
+                      shoulder_label_nudge_x=-0.1*s,
                       summary_statistic=summary_statistic,
                       state_labels=st_lbl,
                       state_colors=st_colors,
                       node_label_size=2,
                       node_size_range=c(2,2),
+                      node_label_nudge_x=0.1*s,
                       tip_label_size=2,
-                      tip_label_offset=0.5,
-                      xlim_visible=c(0,6),
-                      alpha=0.5,
-                      show_posterior_legend=F)
+                      tip_label_offset=s*0.5,
+                      xlim_visible=c(0,s*17),
+                      show_posterior_legend=F,
+                      show_tree_scale=T)
