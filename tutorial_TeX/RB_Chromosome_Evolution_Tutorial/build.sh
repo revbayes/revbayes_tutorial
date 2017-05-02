2ba25fa3e6ec5f1682c54dd5c96b81b205661224
Will Freyman <willfreyman@gmail.com>
2017-05-01 22:56:55 -0700

    part 1 of chromosome tutorial

---
+++ tutorial_TeX/RB_Chromosome_Evolution_Tutorial/build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+tutorial="Chromosome_Evolution_Tutorial"
+
+pdflatex RB_${tutorial}.tex
+bibtex RB_${tutorial}
+pdflatex RB_${tutorial}.tex
+pdflatex RB_${tutorial}.tex
+
+rm RB_${tutorial}.aux
+rm RB_${tutorial}.bbl
+rm RB_${tutorial}.blg
+rm RB_${tutorial}.log
+rm RB_${tutorial}.out
+
