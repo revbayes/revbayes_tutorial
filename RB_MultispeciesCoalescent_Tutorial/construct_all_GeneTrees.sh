4124134ec03528f9e6fd0a955d63ca4923cc1e46
boussau <boussau@gmail.com>
2017-08-10 23:31:55 +0200

    Adding scripts to produce initial gene trees as well as the initial gene trees themselves.

---
+++ RB_MultispeciesCoalescent_Tutorial/construct_all_GeneTrees.sh
@@ -0,0 +1,71 @@
+
+
+# construct the command string
+rb_command="i = 1;
+            source(\"scripts/mcmc_GeneTree.Rev\");"
+
+# pipe the command into RevBayes
+echo $rb_command | rb&
+
+# construct the command string
+rb_command="i = 2;
+            source(\"scripts/mcmc_GeneTree.Rev\");"
+
+# pipe the command into RevBayes
+echo $rb_command | rb&
+
+# construct the command string
+rb_command="i = 3;
+            source(\"scripts/mcmc_GeneTree.Rev\");"
+
+# pipe the command into RevBayes
+echo $rb_command | rb&
+
+# construct the command string
+rb_command="i = 4;
+            source(\"scripts/mcmc_GeneTree.Rev\");"
+
+# pipe the command into RevBayes
+echo $rb_command | rb&
+
+# construct the command string
+rb_command="i = 5;
+            source(\"scripts/mcmc_GeneTree.Rev\");"
+
+# pipe the command into RevBayes
+echo $rb_command | rb&
+
+# construct the command string
+rb_command="i = 6;
+            source(\"scripts/mcmc_GeneTree.Rev\");"
+
+# pipe the command into RevBayes
+echo $rb_command | rb&
+
+# construct the command string
+rb_command="i = 7;
+            source(\"scripts/mcmc_GeneTree.Rev\");"
+
+# pipe the command into RevBayes
+echo $rb_command | rb&
+
+# construct the command string
+rb_command="i = 8;
+            source(\"scripts/mcmc_GeneTree.Rev\");"
+
+# pipe the command into RevBayes
+echo $rb_command | rb&
+
+# construct the command string
+rb_command="i = 9;
+            source(\"scripts/mcmc_GeneTree.Rev\");"
+
+# pipe the command into RevBayes
+echo $rb_command | rb&
+
+# construct the command string
+rb_command="i = 10;
+            source(\"scripts/mcmc_GeneTree.Rev\");"
+
+# pipe the command into RevBayes
+echo $rb_command | rb&
